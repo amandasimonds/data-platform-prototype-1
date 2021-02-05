bb57f8d62ea442a82a9b34a03ab22535502f0501
Kevin Dornellas <kevin.dornellas@ihsmarkit.com>
2021-02-05 08:59:03 -0700

    test coverage

--- projects/uv-ui-lib/src/lib/components/button/button.component.spec.ts
+++ projects/uv-ui-lib/src/lib/components/button/button.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ButtonComponent } from './button.component';
 
@@ -16,10 +17,36 @@ describe('ButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+    component.btnText = 'test text';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit an event when the button is clicked', () => {
+    spyOn(component.onClick, 'emit');
+    component.onClickButton({test:'test'});
+    expect(component.onClick.emit).toHaveBeenCalledOnceWith({test:'test'});
+  });
+
+  it('should have the proper text', () => {
+    const button = fixture.debugElement.query(By.css('a')).nativeElement;
+    expect(button.innerHTML).toBe('test text');
+  });
+
+  it('should have a button with a click event', () => {
+    spyOn(component.onClick, 'emit');
+    const button = fixture.debugElement.query(By.css('a')).nativeElement;
+    button.click();
+    expect(component.onClick.emit).toHaveBeenCalledTimes(1);
+  });
+
+  it('should have the proper classes', () => {
+    component.buttonConfig = {disabled: true};
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('.disabled'));
+    expect(button).toBeTruthy();
   });
 });
