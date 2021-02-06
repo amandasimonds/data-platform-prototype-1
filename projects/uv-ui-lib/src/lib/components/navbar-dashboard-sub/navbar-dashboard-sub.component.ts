5036a59c22aba92ca7e1246288b4ea8d4e2367b2
amandasimonds <amandasimonds9@gmail.com>
2021-02-05 18:22:28 -0700

    uv icons on navbar - changed the path ids in the svg code

---
+++ projects/uv-ui-lib/src/lib/components/navbar-dashboard-sub/navbar-dashboard-sub.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-navbar-dashboard-sub',
+  templateUrl: './navbar-dashboard-sub.component.html',
+  styleUrls: ['../../scss/_styles.scss']
+})
+export class NavbarDashboardSubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
