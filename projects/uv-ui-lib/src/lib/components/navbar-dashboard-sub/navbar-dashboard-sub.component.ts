cba6dec3c80d1dbbf67406de4bfcd84dd29ae658
amandasimonds <amandasimonds9@gmail.com>
2021-02-08 16:53:22 -0700

    tweaked styling on dashboard components, added flex container wrapper around dashboard components, added utils scss file for flex containers, etc

--- projects/uv-ui-lib/src/lib/components/navbar-dashboard-sub/navbar-dashboard-sub.component.ts
+++ projects/uv-ui-lib/src/lib/components/navbar-dashboard-sub/navbar-dashboard-sub.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-navbar-dashboard-sub',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../scss/_styles.scss']
 })
 export class NavbarDashboardSubComponent implements OnInit {
+
+  @Input() headingTxt : string;
 
   constructor() { }
 
