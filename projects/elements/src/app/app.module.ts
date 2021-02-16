aceb2ad1ea28ae52dc1504f844792a7c5543f048
Kevin Dornellas <kevin.dornellas@ihsmarkit.com>
2021-02-16 16:57:46 -0700

    really this time elements

--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector  } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
-import { UvUiLibModule, ɵd } from  'uv-ui-lib';
+import { LoginComponent } from 'projects/uv-ui-lib/src/lib/components/login/login.component';
 
 @NgModule({
   declarations: [
   ],
   imports: [
-    BrowserModule,
-    UvUiLibModule
+    BrowserModule
   ],
   providers: []
 })
@@ -16,7 +15,7 @@ export class AppModule {
   constructor(private injector: Injector){}
 
   ngDoBootstrap(){
-    const element = createCustomElement(ɵd, { injector: this.injector })
-    customElements.define("lib-components", element);
+    const element = createCustomElement(LoginComponent, { injector: this.injector })
+    customElements.define("ihsm-login", element);
   }
 }
