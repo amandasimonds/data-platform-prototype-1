fc0655da6b6f842677a0a849894332a81b7e0073
Kevin Dornellas <kevin.dornellas@ihsmarkit.com>
2021-02-16 16:57:46 -0700

    really this time elements

--- react-app/test-app/src/App.js
+++ react-app/test-app/src/App.js
@@ -6,7 +6,6 @@ class App extends React.Component{
 
   constructor(props) {
     super(props);
-    // Don't do this!
     this.state = 'text';
   }
 
@@ -21,19 +20,7 @@ class App extends React.Component{
     return  (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <lib-components  ref={elem => this.nv = elem} header={this.state}></lib-components>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ihsm-login ref={elem => this.nv = elem} header={this.state}></ihsm-login>
         </header>
       </div>
     );
