345e15f270abfc2d94c418f4e10c4001e18e7868
amandasimonds <amandasimonds9@gmail.com>
2021-02-18 16:00:29 -0700

    added tests to button component and working on header-dashboard tests

--- projects/uv-ui-lib/src/lib/components/header-dashboard/header-dashboard.component.spec.ts
+++ projects/uv-ui-lib/src/lib/components/header-dashboard/header-dashboard.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HeaderDashboardComponent } from './header-dashboard.component';
 
@@ -16,10 +17,16 @@ describe('HeaderDashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderDashboardComponent);
     component = fixture.componentInstance;
+    component.headerText = "test header text";
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the proper text in the header', () => {
+    const header = fixture.debugElement.query(By.css('h1')).nativeElement;
+    expect(header.innerHTML).toBe('test header text');
   });
 });
--- projects/uv-ui-lib/src/lib/components/search-panel/search-panel.component.spec.ts
+++ projects/uv-ui-lib/src/lib/components/search-panel/search-panel.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { SearchPanelComponent } from './search-panel.component';
 
 describe('SearchPanelComponent', () => {
@@ -16,10 +16,25 @@ describe('SearchPanelComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchPanelComponent);
     component = fixture.componentInstance;
+    component.searchBtnClicked = 'searchBtnClicked()';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // it('should have a search button', () => {
+  //   const searchButton = fixture.debugElement.query(By.css('search-panel__icon-search-container')).nativeElement;
+  //   // const searchButton = fixture.debugElement.nativeElement.querySelector('#search');
+  //   expect(searchButton).toBeTruthy();
+  // });
+
+  it('should have a search button with a click listener', () => {
+    spyOn(component, 'searchBtnClicked');
+    const searchButton = fixture.debugElement.query(By.css('search-panel__icon-search-container')).nativeElement;
+    // const searchButton = fixture.debugElement.nativeElement.querySelector('#search');
+    searchButton.click();
+    expect(component.searchBtnClicked).toHaveBeenCalledTimes(1);
   });
 });
