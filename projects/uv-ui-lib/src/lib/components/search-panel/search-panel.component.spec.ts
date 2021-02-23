bd6b75b649a7bf98d70a20660540272468ebfe6f
Arthi Kumar <arthi.kumar@ihsmarkit.com>
2021-02-22 17:14:50 -0700

    Added search panel unit test

--- projects/uv-ui-lib/src/lib/components/search-panel/search-panel.component.spec.ts
+++ projects/uv-ui-lib/src/lib/components/search-panel/search-panel.component.spec.ts
@@ -16,7 +16,9 @@ describe('SearchPanelComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchPanelComponent);
     component = fixture.componentInstance;
-    component.searchBtnClicked = 'test function call';
+    component.searchBtnClicked = '';
+    component.expandFiltersClicked = '';
+    component.expandFiltersContainer = () => {};
     fixture.detectChanges();
   });
 
@@ -31,11 +33,22 @@ describe('SearchPanelComponent', () => {
     expect(searchBtnClickedMock).toHaveBeenCalled();
   });
 
-  it('should have a search icon that is clickable', () => {
-    const searchBtnClickedMock = spyOn(component, 'searchBtnClicked');
-    const searchBtn = fixture.debugElement.nativeElement.querySelector('#search-button');
-    searchBtn.click();
-    expect(searchBtnClickedMock).toHaveBeenCalled();
+
+  it('should have a left arrow icon that is clickable', () => {
+    const expandFiltersClickedMock = spyOn(component, 'expandFiltersClicked');
+    const leftBtn = fixture.debugElement.nativeElement.querySelector('#leftArrow-button');
+    leftBtn.click();
+    expect(expandFiltersClickedMock).toHaveBeenCalled();
   });
-  
+
+  // it('should have a left arrow icon thats expands the filter container on selection', () => {
+  //   const expandFiltersContainerClickedMock = spyOn(component, 'expandFiltersClicked');
+  //   const leftBtn = fixture.debugElement.nativeElement.querySelector('#leftArrow-button');
+  //   const searchPanelFiltersContainer = fixture.debugElement.nativeElement.querySelector('#search-panel-filters-container');
+  //   leftBtn.click();
+  //   // expect(".search-panel__filters-container--expanded").toBeTruthy();
+  //   expect(searchPanelFiltersContainer).toHaveClass('search-panel__filters-container.search-panel__filters-container--expanded');
+  //   // expect(leftArrowBtn).toHaveBeenCalled();
+  // });
+
 });
