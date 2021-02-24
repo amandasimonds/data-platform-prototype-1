29cea185ca1dd7389fd36cc4ad46a9c691308590
amandasimonds <amandasimonds9@gmail.com>
2021-02-24 09:17:52 -0700

    added renderer2 to add id to lib-icons component, added widget component to demo for Devi
    Co-authored-by: Arthi Kumar <arthi.kumar@ihsmarkit.com>
    Co-authored-by: Ben Alexander <ben.alexander@ihsmarkit.com>

---
+++ projects/uv-ui-lib/src/lib/components/widget/widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['../../scss/_styles.scss']
+})
+export class WidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
