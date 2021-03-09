719e29d3ad35a84360cb687c1a705f347808a5d8
amandasimonds <amandasimonds9@gmail.com>
2021-03-09 15:08:27 -0700

    added widget component and logo splash block and home container, updated npm library with these changes

---
+++ projects/uv-ui-lib/src/lib/components/logo-splash-block/logo-splash-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-logo-splash-block',
+  templateUrl: './logo-splash-block.component.html',
+  styleUrls: ['../../scss/_styles.scss']
+})
+export class LogoSplashBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,30 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from "./login/login.component";
 import { StartPageComponent } from "./start-page/start-page.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/login", pathMatch: "full"},
+  {
+    path: "", redirectTo: "/login", pathMatch: "full"
+  },
   {
     path: "",
     component: AppComponent,
     children: [
-      {path: "login", component: LoginComponent},
-      {path: "start-page", component: StartPageComponent}
+      {
+        path: "login",
+        component: LoginComponent
+      },
+      {
+        path: "start-page",
+        component: StartPageComponent,
+        children: [
+          {
+            path: "home",
+            component: HomeComponent
+          }
+        ]
+      },
     ]
   }
 ];
