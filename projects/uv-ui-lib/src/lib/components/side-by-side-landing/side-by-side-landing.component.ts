9a46a158a1f536a5412a6f2902ede1a1a9b1cac0
amandasimonds <amandasimonds9@gmail.com>
2021-03-12 10:38:03 -0700

    added a dropdown type button streamlined button scss header shrinks correctly with screen resolution changed flex container to fixed container for the app wrapper container

--- projects/uv-ui-lib/src/lib/components/side-by-side-landing/side-by-side-landing.component.ts
+++ projects/uv-ui-lib/src/lib/components/side-by-side-landing/side-by-side-landing.component.ts
@@ -8,6 +8,8 @@ import { Component, Input} from '@angular/core';
 export class SideBySideLandingComponent {
 
   @Input() tasks : Task
+  @Input() addTaskClicked : any
+  @Input() switchFilterClicked : any
 
   constructor() { }
 
