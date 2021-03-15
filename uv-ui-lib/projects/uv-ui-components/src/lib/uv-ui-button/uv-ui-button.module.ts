f82056bec9bafa14186eb538a7b47f25a1412c8d
Hrossu, Vladyslav <hrossu.vladyslav@ihsmarkit.com>
2021-03-15 13:48:36 +0000

    feat(front): #no-task Added library project with storybook
    - New angular library app
    - Storybook connection
    - Eslint configuration
    - CommitMessageLint configuration
    - Stylelint configuration
    - Editorconfig configuration
    - Git Hooks linting configuration

---
+++ uv-ui-lib/projects/uv-ui-components/src/lib/uv-ui-button/uv-ui-button.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UvUiButtonComponent } from './uv-ui-button.component';
+
+@NgModule({
+    declarations: [ UvUiButtonComponent ],
+    imports: [
+        CommonModule
+    ],
+    exports: [ UvUiButtonComponent ]
+})
+export class UvUiButtonModule {
+}
