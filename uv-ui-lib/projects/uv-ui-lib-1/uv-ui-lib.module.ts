d5cab815ba34db476051353a67ec4b167fa44f2b
amandasimonds <amandasimonds9@gmail.com>
2021-03-19 13:30:39 -0600

    fixed lint errors on icon component, moved old components into a different folder under project

--- uv-ui-lib/projects/uv-ui-lib-1/uv-ui-lib.module.ts
+++ uv-ui-lib/projects/uv-ui-lib-1/uv-ui-lib.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 import { LoginComponent } from './login/login.component';
 import { ButtonComponent } from './button/button.component';
 import { NavbarDashboardComponent } from './navbar-dashboard/navbar-dashboard.component';
