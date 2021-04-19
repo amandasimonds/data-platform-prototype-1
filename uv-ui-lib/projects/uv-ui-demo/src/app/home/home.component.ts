a9b883ccd0bfda3f6773fe142eeb7e28e682136c
amandasimonds <amandasimonds9@gmail.com>
2021-04-19 15:46:11 -0600

    fixed nav variable in start-page.component.ts, fixed selector names

--- uv-ui-lib/projects/uv-ui-demo/src/app/home/home.component.ts
+++ uv-ui-lib/projects/uv-ui-demo/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-uv-ui-home',
+  selector: 'demo-uv-ui-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
