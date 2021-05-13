0b9dce79feee295ba2ade4933a4bf5e7546a051d
amandasimonds <amandasimonds9@gmail.com>
2021-05-13 09:20:37 -0600

    added more search results, changed scrollbar end increment thing, deleted commented code

--- uv-ui-lib/projects/uv-ui-prototype/src/app/services/header.service.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/services/header.service.ts
@@ -8,18 +8,6 @@ export class HeaderService {
   public title = new BehaviorSubject('Title');
   public appIcon = new BehaviorSubject('assets/search.svg#search');
 
-  constructor() { }
-
-    // // getting the title
-    // getTitle(): string {
-
-    // }
-    
-    // // setting the ttile
-    // setTitle(newTitle: string) {
-
-    // }
-
   setTitle(title: string) {
     this.title.next(title);
     console.log("header service", title)
