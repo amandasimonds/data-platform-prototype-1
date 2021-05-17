40be5b2699ae77b830d145ea7610753f8dd740d2
amandasimonds <amandasimonds9@gmail.com>
2021-05-17 11:10:48 -0600

    slight change to grid layout, added landing page component for search app

--- uv-ui-lib/projects/uv-ui-prototype/src/app/login/login.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/login/login.component.ts
@@ -12,6 +12,6 @@ export class LoginComponent {
   constructor(private route: Router) { }
 
   public login() {
-    this.route.navigate(['/main']);
+    this.route.navigate(['/main/home-splash']);
   }
 }
