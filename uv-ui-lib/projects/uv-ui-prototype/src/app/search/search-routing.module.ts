f49e94b843cade35d8c22949a22c8c692d4525f6
amandasimonds <amandasimonds9@gmail.com>
2021-05-17 11:10:48 -0600

    slight change to grid layout, added landing page component for search app

--- uv-ui-lib/projects/uv-ui-prototype/src/app/search/search-routing.module.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/search/search-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { SearchComponent } from './search.component';
 
 const routes: Routes = [
     { path: '', component: SearchComponent, children: [
+        { path: '', component: LandingComponent },
         { path: 'search-results', component: SearchResultsComponent}
     ] },
     
