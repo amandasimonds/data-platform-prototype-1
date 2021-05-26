96a179279ff63fd612790cf3c98bce764143ebea
amandasimonds <amandasimonds9@gmail.com>
2021-05-26 09:54:51 -0600

    added angular material for drag/drop components, created full and basic mode for search component

--- uv-ui-lib/projects/uv-ui-prototype/src/app/search/search.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/search/search.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component,  ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { HeaderService } from '../services/header.service';
 import { SearchResult } from './models/search-result.model';
@@ -19,11 +20,19 @@ export class SearchComponent implements OnInit {
     public searchTabs = DEFAULT_SEARCH_MENU_STATE.menuTabs;
     categorySelected = true;
     public reOrderModeActivated = false;
-    public filterBarActive = false;
+    public filterBarActive = true;
 
     constructor(private headerService: HeaderService){
         this.categorySelected = false;
     }
+
+    items = [
+        {title: "Checkbox"}
+    ]
+
+    drop(event: CdkDragDrop<string[]>) {
+        moveItemInArray(this.searchTabs, event.previousIndex, event.currentIndex);
+      }
 
     ngOnInit(): void {
         this.headerService.setTitle('Search');
