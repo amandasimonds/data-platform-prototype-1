4b4057706b5b32f3a61257a9df85aa2a53cc63ad
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-06-03 14:23:07 -0600

    moved stories files into their respective folders, made a folder structure in Storybook instance

--- uv-ui-lib/projects/uv-ui-lib/src/lib/button/button.stories.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/button/button.stories.ts
@@ -1,9 +1,9 @@
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
 import { Meta, Story } from '@storybook/angular/types-6-0';
-import { ButtonComponent } from '../projects/uv-ui-lib/src/lib/button/button.component';
+import { ButtonComponent } from './button.component';
 
 export default {
-    title: 'Example/ButtonComponent',
+    title: 'Components/Buttons/Button',
     component: ButtonComponent,
     argTypes: {
         backgroundColor: { control: 'color' },
@@ -15,32 +15,32 @@ const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
     props: args,
 });
 
-export const btn = Template.bind({});
-btn.args = {
-    buttonText: 'new button',
+export const Primary = Template.bind({});
+Primary.args = {
+    label: 'Button',
     type: 'btn'
 };
 
 export const login = Template.bind({});
 login.args = {
-    buttonText: 'login button',
+    label: 'login button',
     type: 'btn-login'
 };
 
 export const chip = Template.bind({});
 chip.args = {
-    buttonText: 'chip button',
+    label: 'chip button',
     type: 'btn-chip'
 };
 
 export const blueBtn = Template.bind({});
 blueBtn.args = {
-    buttonText: 'blue button',
+    label: 'blue button',
     type: 'btn-blue'
 };
 
 export const roundBtn = Template.bind({});
 roundBtn.args = {
-    buttonText: '+',
+    label: '+',
     type: 'btn-round'
 };
--- uv-ui-lib/projects/uv-ui-lib/src/lib/svg-button/svg-button.component.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/svg-button/svg-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-uv-ui-svg-button',
@@ -6,20 +6,10 @@ import { Component, ChangeDetectionStrategy, Input, OnInit } from '@angular/core
   styleUrls: ['./svg-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SvgButtonComponent implements OnInit {
+export class SvgButtonComponent {
     @Input() public svgPath = '';
     @Input() public color: 'gray' | 'blue' | 'black' = 'black';
     @Input() public size: 'small' | 'xs' | 'xxs' = 'small';
-
-    // public cssClasses = ['btn', 'btn-svg'];
-
-    public getCssClasses(): void {
-        this.cssClasses.push(this.color, this.size);
-    }
-
-    public ngOnInit(): void {
-        this.getCssClasses();
-    }
 
     public get cssClasses(): string[] {
 
