66a50692272643ffa10352405e28e94ce8f1e414
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-06-16 18:20:11 -0600

    fixed spacing, added and used w-100, position-relative class, fixed static classes in ngClass, property binding, deleted commented code, deleted npmrc file

--- uv-ui-lib/projects/uv-ui-lib/src/lib/checkbox/checkbox.component.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/checkbox/checkbox.component.ts
@@ -8,31 +8,30 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 })
 export class CheckboxComponent {
 
-  @Input() public label = '';
-  @Input() public disabled = false;
-
-  public isChecked = false;
-  public onChange: any = () => {};
-
-  @Input() public set value(isChecked: boolean) {
-    if (!this.disabled) {
-      this.isChecked = isChecked;
-      this.onChange(isChecked);
+    @Input() public label = '';
+    @Input() public disabled = false;
+    @Input() public set value(isChecked: boolean) {
+        if (!this.disabled) {
+            this.isChecked = isChecked;
+            this.onChange(isChecked);
+        }
     }
-  }
+
+    public isChecked = false;
+    public onChange: any = () => {};
 
     public writeValue(checked: boolean): void {
-    this.isChecked = checked;
-  }
-
-  public setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-
-  public toggleChecked(isChecked: boolean): void {
-    if (!this.disabled) {
-      this.isChecked = !this.isChecked;
-      this.onChange(this.isChecked);
+        this.isChecked = checked;
     }
-  }
+
+    public setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+
+    public toggleChecked(isChecked: boolean): void {
+        if (!this.disabled) {
+            this.isChecked = !this.isChecked;
+            this.onChange(this.isChecked);
+        }
+    }
 }
