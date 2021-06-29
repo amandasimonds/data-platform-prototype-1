99c59c8b2f2cdaffb932adfc2956b9a22c22eeff
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-06-29 12:52:40 -0600

    uv-app-shell added to storybook

--- uv-ui-lib/projects/uv-ui-lib/src/lib/sidebar/app-side-bar.module.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/sidebar/app-side-bar.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ButtonModule } from '../button/button.module';
-import { SvgButtonModule } from '../svg-button/svg-button.module';
-import { AppSideBarComponent } from './app-side-bar.component';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { ButtonModule } from '../button/button.module';
+// import { SvgButtonModule } from '../svg-button/svg-button.module';
+// import { AppSideBarComponent } from './app-side-bar.component';
 
-@NgModule({
-    declarations: [ AppSideBarComponent],
-    imports: [
-        CommonModule, ButtonModule, SvgButtonModule
-    ],
-    exports: [ AppSideBarComponent ]
-})
-export class AppSideBarModule {
-}
+// @NgModule({
+//     declarations: [ AppSideBarComponent],
+//     imports: [
+//         CommonModule, ButtonModule, SvgButtonModule
+//     ],
+//     exports: [ AppSideBarComponent ]
+// })
+// export class AppSideBarModule {
+// }
