eac2d03a551c77850f9594aa529c5e1c3da1fbf9
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-07-06 14:15:28 -0600

    added storySort option on preview.js, took out deprecated components from storybook, changed naming of button modes to match the latest changes from UI design

--- uv-ui-lib/projects/uv-ui-lib/src/lib/button/button.component.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/button/button.component.ts
@@ -8,7 +8,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
     @Input() public label = 'Button';
-    @Input() public mode: 'basic' | 'primary' | 'round' = 'basic';
+    @Input() public mode: 'text' | 'primary' | 'round' = 'text';
     @Input() public icon = '';
     @Input() public disabled = false;
 }
