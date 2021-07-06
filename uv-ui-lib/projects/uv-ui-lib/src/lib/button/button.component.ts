067823bf262ceee51f4c28733be043a859fce229
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-07-06 09:35:46 -0600

    added card component and story, changed selector name to more simple name for button

--- uv-ui-lib/projects/uv-ui-lib/src/lib/button/button.component.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/button/button.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'lib-uv-ui-button',
+    selector: 'uv-button',
     templateUrl: './button.component.html',
     styleUrls: ['./button.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
