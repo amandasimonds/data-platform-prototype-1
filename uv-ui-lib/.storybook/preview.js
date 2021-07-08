348c51604d78c33fb8ba666ab11756467463349b
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-07-08 11:01:43 -0600

    added typography and color theme stories, included background colors in storybook preview file, tweaked typography, added gap-10 utility class for flexbox

--- uv-ui-lib/.storybook/preview.js
+++ uv-ui-lib/.storybook/preview.js
@@ -1,11 +1,28 @@
-
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import '!style-loader!css-loader!sass-loader!./../projects/uv-ui-lib/src/lib/scss/_styles.scss';
+import '!style-loader!css-loader!sass-loader!./styles.scss';
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: 'white',
+      },
+      {
+        name: 'gray',
+        value: 'var(--uv-background-color)',
+      },
+      {
+        name: 'dark',
+        value: 'var(--uv-background-color)',
+      },
+    ],
+  },
   options: {
     storySort: {
       order: [
