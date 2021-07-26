0355b88e155f3e37acff78fac9e0ea56c8ace2fe
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-07-26 09:37:15 -0600

    deleted uv-ui-components

--- uv-ui-lib/projects/uv-ui-components/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of uv-ui-components
- */
-
-export * from '../../uv-ui-lib/src/lib/uv-ui-button/uv-ui-button.component';
-export * from '../../uv-ui-lib/src/lib/uv-ui-button/uv-ui-button.module';
