d7ce052500e102026cdda456a95928a85a3ec355
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-07-26 16:25:30 -0600

    traded auth service for oidc library

---
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/services/on-destroy.service.ts
@@ -0,0 +1,12 @@
+﻿import { Injectable, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class NgOnDestroyService extends Subject<null> implements OnDestroy {
+    // TODO: add explicit constructor
+
+    public ngOnDestroy(): void {
+        this.next(null);
+        this.complete();
+    }
+}
