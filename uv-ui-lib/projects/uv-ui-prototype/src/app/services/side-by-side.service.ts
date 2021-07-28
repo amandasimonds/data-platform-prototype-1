973e241b642d3ca72e22275f06f349da377bcf7b
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-07-28 14:34:44 -0600

    added disabled and hover state to icon button component, added behaviorsubject/observables to sidebyside.service, added some classes to utils.scss, moved scss to central sbs.scss location

--- uv-ui-lib/projects/uv-ui-prototype/src/app/services/side-by-side.service.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/services/side-by-side.service.ts
@@ -1,4 +1,5 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { ISbsSourceDocument } from '../side-by-side/models/sbs-source-document.model';
 import { ISbsTargetDocument } from '../side-by-side/models/sbs-target-document.model';
 import { sourceDocumentSamples } from '../side-by-side/sample-data/source-documents';
@@ -7,17 +8,30 @@ import { targetDocumentSamples } from '../side-by-side/sample-data/target-docume
 @Injectable()
 export class SidebySideService {
 
-    targetDocumentSelected = new EventEmitter<ISbsTargetDocument>();
-    sourceDocumentSelected = new EventEmitter<ISbsSourceDocument>();
+    private targetDocumentSelectedEvent = new BehaviorSubject<ISbsTargetDocument[]>([]);
+    private sourceDocumentSelectedEvent = new BehaviorSubject<ISbsSourceDocument[]>([]);
+    readonly selectedTargetDocuments$ = this.targetDocumentSelectedEvent.asObservable();
+    readonly sourceDocuments$ = this.sourceDocumentSelectedEvent.asObservable();
 
     public targetDocuments: ISbsTargetDocument[] = targetDocumentSamples;
     public sourceDocuments: ISbsSourceDocument[] = sourceDocumentSamples;
 
     public getTargetDocuments(): ISbsTargetDocument[]{
-        return this.targetDocuments.slice();
+        return this.targetDocumentSelectedEvent.value.slice();
     }
 
     public getSourceDocuments(): ISbsSourceDocument[]{
         return this.sourceDocuments.slice();
     }
+
+    public selectSourceDocuments(document: ISbsSourceDocument){
+        const sourceDocs = [...this.sourceDocumentSelectedEvent.value, document]
+        this.sourceDocumentSelectedEvent.next(sourceDocs);
+    }
+
+    public selectTargetDocument(document: ISbsTargetDocument){
+        const selectedTargetDocs = [...this.targetDocumentSelectedEvent.value, document]
+        this.targetDocumentSelectedEvent.next(selectedTargetDocs);
+    }
+
 }
--- uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/target-document/target-document.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/target-document/target-document.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { SidebySideService } from '../../services/side-by-side.service';
 import { ISbsTargetDocument } from '../models/sbs-target-document.model';
 import { targetDocumentSamples } from '../sample-data/target-documents';
 
 @Component({
   selector: 'app-target-document',
   templateUrl: './target-document.component.html',
-  styleUrls: ['../source-document/source-document.component.scss', './target-document.component.scss'],
+  styleUrls: ['./target-document.component.scss', '../side-by-side.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TargetDocumentComponent implements OnInit {
+export class TargetDocumentComponent {
 
     public targetDocuments: ISbsTargetDocument[] = targetDocumentSamples;
-    @Output() onSourceDocumentSelected = new EventEmitter<any>();
+
+    @Output() selectedTargetDocument = new EventEmitter<ISbsTargetDocument>();
+
+    constructor(private sbsService: SidebySideService) {}
 
     onTargetDocumentSelected(item: ISbsTargetDocument){
         item.active = !item.active;
-        this.onSourceDocumentSelected.emit(item);
+        if(item.active === true){
+            this.sbsService.selectTargetDocument(item);
+        }
     }
-
-    ngOnInit(): void {}
-
 }
