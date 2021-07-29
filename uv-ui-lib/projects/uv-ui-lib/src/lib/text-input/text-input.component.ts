0bd16a553b5aeb0ba9f71120b16ac1493cd4c8ed
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-07-29 12:38:35 -0600

    added text-input component, moved input styling from search-panel to global typography.scss, added utility classes, added active state to icon button, added filter-modal component to side by side

---
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/text-input/text-input.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'uvx-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.scss']
+})
+export class TextInputComponent {
+
+ @Input() placeholder = '';
+
+}
--- uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/sbs-action-bar/sbs-action-bar.module.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/sbs-action-bar/sbs-action-bar.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActionBarModule, ButtonModule, IconButtonModule, UvxIconModule } from 'projects/uv-ui-lib/src/public-api';
+import { 
+    ActionBarModule, 
+    ButtonModule, 
+    IconButtonModule, 
+    UvxIconModule } from 'projects/uv-ui-lib/src/public-api';
 import { SbsActionBarComponent } from './sbs-action-bar.component';
+import { FilterModalModule } from '../filter-modal/filter-modal.module';
 
 @NgModule({
   declarations: [SbsActionBarComponent],
@@ -10,7 +15,8 @@ import { SbsActionBarComponent } from './sbs-action-bar.component';
     ButtonModule,
     ActionBarModule,
     IconButtonModule,
-    UvxIconModule
+    UvxIconModule,
+    FilterModalModule
   ],
   exports: [ SbsActionBarComponent]
 })
