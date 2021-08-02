9e3f043883dfbc6a4975498a50886b88c5f3b78c
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-08-02 11:19:29 -0600

    fixed expanding box in action bar, made document component to house target and source document components, moved scss around

--- uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/sbs-document/source-document/source-document.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/sbs-document/source-document/source-document.component.ts
@@ -1,12 +1,12 @@
 import { Component, ChangeDetectionStrategy, Output } from '@angular/core';
-import { SidebySideService } from '../../services/side-by-side.service';
-import { ISbsSourceDocument } from '../models/sbs-source-document.model';
-import { sourceDocumentSamples } from '../sample-data/source-documents';
+import { SidebySideService } from '../../../services/side-by-side.service';
+import { ISbsSourceDocument } from '../../models/sbs-source-document.model';
+import { sourceDocumentSamples } from '../../sample-data/source-documents';
 
 @Component({
   selector: 'app-source-document',
   templateUrl: './source-document.component.html',
-  styleUrls: ['./source-document.component.scss', '../side-by-side.component.scss'],
+  styleUrls: ['./source-document.component.scss', '../../side-by-side.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SourceDocumentComponent {
