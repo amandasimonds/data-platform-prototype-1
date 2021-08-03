92831fc1bc2a06e7a6afe4749da24f67018bd083
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-08-03 08:52:57 -0600

    show more button on document card, updated scss files

--- uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/sbs-document/source-document/source-document.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/sbs-document/source-document/source-document.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Output } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Output, Input, EventEmitter } from '@angular/core';
 import { SidebySideService } from '../../../services/side-by-side.service';
 import { ISbsSourceDocument } from '../../models/sbs-source-document.model';
 import { sourceDocumentSamples } from '../../sample-data/source-documents';
@@ -6,12 +6,14 @@ import { sourceDocumentSamples } from '../../sample-data/source-documents';
 @Component({
   selector: 'app-source-document',
   templateUrl: './source-document.component.html',
-  styleUrls: ['./source-document.component.scss', '../../side-by-side.component.scss'],
+  styleUrls: ['../sbs-document.component.scss', '../../side-by-side.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SourceDocumentComponent {
 
     public sourceDocuments: ISbsSourceDocument[] = sourceDocumentSamples;
+
+    showMoreDocumentDetails = false;
 
     @Output() selectedSourceDocument: ISbsSourceDocument = {
         id: 1,
@@ -21,10 +23,18 @@ export class SourceDocumentComponent {
         tags: []
     };
 
+    @Output() onSourceDocumentClickedEvent: EventEmitter<boolean> = new EventEmitter();
+
+    onShowMoreDocumentDetails() {
+        this.showMoreDocumentDetails = !this.showMoreDocumentDetails;
+        console.log('click event');
+    }
+
     constructor(private sbsService: SidebySideService) { }
 
     onSourceDocumentSelected(item: ISbsSourceDocument){
         this.sbsService.selectSourceDocuments(item);
         item.active = !item.active;
+        this.onSourceDocumentClickedEvent.emit(true);
     }
 }
