0c81e3f1d8dd3cff35360b0d35ba8f55d4c837ba
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-08-03 08:52:57 -0600

    show more button on document card, updated scss files

--- uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/sample-data/source-documents.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/side-by-side/sample-data/source-documents.ts
@@ -4,14 +4,16 @@ export const sourceDocumentSamples = [
         title: 'EXH-SU-5110-B',
         description: 'Lorem upsum door sit amet consectetur',
         active: false,
-        tags: ['Lorem Ipsum', 'Sit amet', 'Lorem, Dolor, and 3 more', 'Lorem', 'Ipsum']
+        tags: ['Lorem Ipsum', 'Sit amet', 'Lorem, Dolor, and 3 more', 'Lorem', 'Ipsum', 'Lorem Ipsum', 'Sit Amet', 'Lorem', 'Lorem, Dolor, and 3 more'],
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
         id: 2,
         title: 'EXH-SU-5110-B',
         description: 'Lorem upsum door sit amet consectetur',
         active: false,
-        tags: ['Lorem Ipsum', 'Sit amet', 'Lorem, Dolor, and 3 more', 'Lorem', 'Ipsum']
+        tags: ['Lorem Ipsum', 'Sit amet', 'Lorem, Dolor, and 3 more', 'Lorem', 'Ipsum'],
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
         id: 3,
