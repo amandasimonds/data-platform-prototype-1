5fa295faec282e6baa9ea5596ae268a2c4371ff6
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-08-18 21:19:24 -0600

    backdrop service idea

--- uv-ui-lib/projects/uv-ui-prototype/src/app/search/search-sidebar/search-sidebar.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/search/search-sidebar/search-sidebar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { BackdropService } from '../../services/backdrop.service';
 
 @Component({
-  selector: 'app-search-sidebar',
-  templateUrl: './search-sidebar.component.html',
-  styleUrls: ['./search-sidebar.component.scss']
+    selector: 'app-search-sidebar',
+    templateUrl: './search-sidebar.component.html',
+    styleUrls: ['./search-sidebar.component.scss']
 })
 export class SearchSidebarComponent implements OnInit {
 
-  constructor() { }
+    constructor(private backdropService: BackdropService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.backdropService.setBackdrop(true);
+    }
 
 }
