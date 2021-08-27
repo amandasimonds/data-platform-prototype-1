0d5de83cf0327f8f201c78cb44e27b5c036df78d
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-08-26 18:46:20 -0600

    search-results data, results styled, tool-menu, fixed scroll on search sidebar, compare modal popup

---
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/search/search-results/sample-search-results/recent-searches.ts
@@ -0,0 +1,35 @@
+export const recentSearches = 
+[
+    {
+        title: 'Lorem ipsum dolor sit amet consectetur',
+        date: 'Today at 08:45 PM'
+    },
+    {
+        title: 'Dolor sit amet consectetur',
+        date: 'Today at 08:42 PM'
+    },
+    {
+        title: 'EXH',
+        date: 'Yesterday at 04:21 PM'
+    },
+    {
+        title: 'EXH 210349',
+        date: '21 June at 04:21 PM'
+    },
+    {
+        title: 'EXH 210251',
+        date: '21 June at 02:15 PM'
+    },
+    {
+        title: 'Dolor sit consectetur',
+        date: '21 June at 02:13 PM'
+    },
+    {
+        title: 'Lorem ipsum dolor sit amet consectetur',
+        date: '19 June at 04:21 PM'
+    },
+    {
+        title: 'Lorem ipsum dolor sit amet consectetur',
+        date: '19 June at 01:12 PM'
+    },
+]
