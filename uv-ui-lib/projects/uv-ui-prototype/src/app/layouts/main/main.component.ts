efd1fa12273a03fbf626d626f2d57ec3d653e4d5
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-08-31 12:23:37 -0600

    sidebar animation working

--- uv-ui-lib/projects/uv-ui-prototype/src/app/layouts/main/main.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/layouts/main/main.component.ts
@@ -1,13 +1,15 @@
 import { Component, ChangeDetectionStrategy, OnInit, ChangeDetectorRef, Input } from '@angular/core';
 import { AppShellService } from '../../services/app-shell.service';
 import { navItems } from './navItems';
-import { SlideInOutAnimation } from '../../animations';
+import { FadeInOutAnimation, SlideInOutAnimation } from '../../animations';
+import { SearchService } from '../../services/search.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'prototype-app-main',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
-    animations: [SlideInOutAnimation],
+    animations: [ FadeInOutAnimation ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 
@@ -18,15 +20,18 @@ export class MainComponent implements OnInit {
     public appIcon = '';
     public activeAppIcon = '';
     public compareWarning = false;
-    // animationState = 'out';
+    public searchSidebarState = 'hidden';
+
     @Input() public searchQuery = '';
-    @Input() showSearchSidebar = false;
 
     constructor(
         public appShellService: AppShellService, 
-        private ref: ChangeDetectorRef) { }
+        public searchService: SearchService,
+        private ref: ChangeDetectorRef,
+        private route: ActivatedRoute) { }
 
     public ngOnInit(): void {
+        console.log(+this.route.snapshot.params['id']);
         this.appShellService.currentTitle$.subscribe(title => {
             this.title = title;
             this.ref.detectChanges();
@@ -39,23 +44,21 @@ export class MainComponent implements OnInit {
             this.activeAppIcon = icon;
             this.ref.detectChanges();
         });
+        this.searchService.searchState$.subscribe(state => {
+            this.searchSidebarState = state;
+            this.ref.detectChanges();
+        });
     }
 
     public get checkifSearch(): boolean {
         return this.title === 'Search';
     }
 
-    consoleLog(item: { app: string; icon: string; active: boolean; navlink: string; }){
-        console.log(item.active);
-    }
-
     toggleSearchSidebar() {
-        this.showSearchSidebar = !this.showSearchSidebar;
-        // console.log(this.animationState);
-    //   this.animationState = this.animationState === 'out' ? 'in' : 'out';
-    //   console.log(this.animationState);
-    }
-
-    ngOnChanges(): void {
+        this.searchSidebarState == 'hidden' ? 
+            (this.searchService.setSearchSidebarState('visible'),
+            this.appShellService.setActiveAppNav('search')) : 
+            (this.searchService.setSearchSidebarState('hidden'),
+            this.appShellService.setActiveAppNav(''));
     }
 }
--- uv-ui-lib/projects/uv-ui-prototype/src/app/services/search.service.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/services/search.service.ts
@@ -2,9 +2,19 @@ import { SearchResult } from '../search/models/search-result.model';
 import { allSearchResults } from '../search/search-results/sample-search-results/allSearchResults';
 import * as configurationsData from '../../sample-data/configurations.json';
 import * as partsData from '../../sample-data/parts.json';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 export class SearchService {
     public allSearchResults: SearchResult[] = allSearchResults;
+    public searchSidebarState = new BehaviorSubject<string>('hidden');
+
+    public get searchState$(): Observable<string> {
+        return this.searchSidebarState.asObservable();
+    }
+
+    public setSearchSidebarState(state: string): void {
+        this.searchSidebarState.next(state);
+    }
 
     public getAllSearchResults(): SearchResult[]{
         return this.allSearchResults.slice();
@@ -22,9 +32,5 @@ export class SearchService {
         );
 
         return results;
-    }
-
-    public toggleSearchSidebar() {
-        
     }
 }
