2f81844a2f3156cf9857076893078885e1c8996b
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-09-02 16:44:46 -0600

    sorting recent searches, fix search panel, added date to recent search

--- uv-ui-lib/projects/uv-ui-prototype/src/app/search/models/search-result.model.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/search/models/search-result.model.ts
@@ -2,10 +2,12 @@ export class SearchResult {
     public category: string;
     public title: string;
     public description: string;
+    public date: string;
 
-    constructor(category: string, title: string, desc: string) {
+    constructor(category: string, title: string, desc: string, date: string) {
         this.category = category;
         this.title = title;
         this.description = desc;
+        this.date = date;
     }
 }
--- uv-ui-lib/projects/uv-ui-prototype/src/app/search/search-results/sample-search-results/allSearchResults.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/search/search-results/sample-search-results/allSearchResults.ts
@@ -3,41 +3,47 @@ export const allSearchResults = [
       category: 'Parts',
       title: 'EXH-SU-5110-B1',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-      active: false
+      active: false,
+      date: ''
     },
     {
       category: 'Parts',
       title: 'EXH-A-SU-5110-B2',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-      active: false
+      active: false,
+      date: ''
     },
 
     {
         category: 'Parts',
         title: 'EXH-B-SU-5110-B1',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-        active: false
+        active: false,
+        date: ''
     },
 
     {
         category: 'Parts',
         title: 'EXH-C-SU-5110-B2',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-        active: false
+        active: false,
+        date: ''
     },
 
     {
         category: 'Parts',
         title: 'EXH-SU-5110-B1',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-        active: false
+        active: false,
+        date: ''
     },
 
     {
       category: 'Parts',
       title: 'EXH-SU-5110-B2',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-      active: false
+      active: false,
+      date: ''
     },
 
     {
@@ -45,117 +51,134 @@ export const allSearchResults = [
       title: 'EXH-SU-5110-B3',
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.` ,
-      active: false
+      active: false,
+      date: ''
     },
     {
       category: 'Parts',
       title: 'PartABCD000004',
       description: 'Screw, Hexagon Flange Head Cap v4.',
-      active: false
+      active: false,
+      date: ''
     },
 
     {
       category: 'Requirements',
       title: 'RequirementsABCD000001',
       description: 'Screw, Hexagon Flange Head Cap v4.',
-      active: false
+      active: false,
+      date: ''
     },
 
     {
       category: 'Requirements',
       title: 'RequirementsABCD000002',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      active: false
+      active: false,
+      date: ''
     },
 
     {
       category: 'Requirements',
       title: 'RequirementsABCD000003',
       description: 'Screw, Hexagon Flange Head Cap v4.1',
-      active: false
+      active: false,
+      date: ''
     },
 
     {
       category: 'Requirements',
       title: 'RequirementsABCD000004',
       description: 'Screw, Hexagon Flange Head Cap v4.1',
-      active: false
+      active: false,
+      date: ''
     },
 
     {
       category: 'Requirements',
       title: 'RequirementsABCD000005',
       description: 'Screw, Hexagon Flange Head Cap v4.1',
-      active: false
+      active: false,
+      date: ''
     },
 
       {
         category: 'Documents',
         title: 'DocumentsABCD000001',
         description: 'Screw, Hexagon Flange Head Cap v4.1',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Documents',
         title: 'DocumentsABCD000002',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Documents',
         title: 'DocumentsABCD000003',
         description: 'Screw, Hexagon Flange Head Cap v4.1',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Documents',
         title: 'DocumentsABCD000004',
         description: 'Screw, Hexagon Flange Head Cap v4.1',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Documents',
         title: 'DocumentsABCD000005',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Documents',
         title: 'DocumentsABCD000006',
         description: 'Screw, Hexagon Flange Head Cap v4.1',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Integrity',
         title: 'IntegrityABCD000003',
         description: 'Screw, Hexagon Flange Head Cap v4.1',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Entity',
         title: 'EntityABCD000004',
         description: 'Screw, Hexagon Flange Head Cap v4.1',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Industry',
         title: 'IndustryABCD000005',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        active: false
+        active: false,
+        date: ''
       },
 
       {
         category: 'Materials',
         title: 'MaterialsABCD000006',
         description: 'Screw, Hexagon Flange Head Cap v4.1',
-        active: false
+        active: false,
+        date: ''
       }
     ];
