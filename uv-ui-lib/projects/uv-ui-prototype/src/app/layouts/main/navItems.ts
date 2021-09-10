043822ab0f5b6df58197943ea09d0f7cf73543ef
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-09-10 15:16:07 -0600

    init - auth and disabled other links

--- uv-ui-lib/projects/uv-ui-prototype/src/app/layouts/main/navItems.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/layouts/main/navItems.ts
@@ -2,32 +2,37 @@ export const navItems = [
     {   app: 'home-splash',
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-quad-gradient.svg',
         active: false,
-        navlink: 'home-splash'
+        navlink: 'home-splash',
+        disabled: false
     },
     {
         app: 'gwu',
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-gwu-gradient.svg',
         active: false,
-        navlink: 'gwu'
+        navlink: '/main',
+        disabled: true
     },
 
     {
         app: 'sbs',
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-sbs-gradient.svg',
         active: false,
-        navlink: 'sbs'
+        navlink: '/main',
+        disabled: true
     },
     {
         app: 'design-ripple',
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-design-ripple-gradient.svg',
         active: false,
-        navlink: '/main'
+        navlink: '/main',
+        disabled: true
     },
     {
         app: 'ingest',
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-ingestion-gradient.svg',
         active: false,
-        navlink: '/main'
+        navlink: '/main',
+        disabled: true
     },
     // {
     //     app: 'settings',
