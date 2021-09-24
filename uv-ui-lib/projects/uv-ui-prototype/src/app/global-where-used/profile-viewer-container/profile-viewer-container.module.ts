6a2528e59ba86e2a5e40eac7417eb8d3c9851dea
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-09-24 10:14:22 -0600

    profile viewer container component, profile viewer component, connected wizard to profile viewer landing, edited scss

---
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/global-where-used/profile-viewer-container/profile-viewer-container.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileViewerContainerComponent } from './profile-viewer-container.component';
+
+
+
+@NgModule({
+  declarations: [ProfileViewerContainerComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [ProfileViewerContainerComponent]
+})
+export class ProfileViewerContainerModule { }
