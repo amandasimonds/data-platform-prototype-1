37b4daf741a07581ef7f42469d7cbc631432d4a3
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-09-27 11:40:20 -0600

    changed nav and auth to be ready for main

--- uv-ui-lib/projects/uv-ui-prototype/src/app/layouts/main/navItems.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/layouts/main/navItems.ts
@@ -10,37 +10,33 @@ export const navItems = [
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-gwu-gradient.svg',
         active: false,
         navlink: 'gwu',
-        // disabled: false,
-        disabled: true
+        disabled: false
     },
     {
         app: 'sbs',
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-sbs-gradient.svg',
         active: false,
         navlink: 'sbs',
-        // disabled: false,
-        disabled: true
+        disabled: false
     },
     {
         app: 'design-ripple',
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-design-ripple-gradient.svg',
         active: false,
         navlink: '/main',
-        // disabled: false,
-        disabled: true
+        disabled: false
     },
     {
         app: 'ingest',
         icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-ingestion-gradient.svg',
         active: false,
         navlink: '/main',
-        // disabled: false,
-        disabled: true
+        disabled: false,
     },
-    // {
-    //     app: 'settings',
-    //     icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-settings-gradient.svg',
-    //     active: false,
-    //     navlink: 'settings'
-    // }
+    {
+        app: 'settings',
+        icon: 'http://dpzaq1wj21co3.cloudfront.net/icons/gradient/apps-settings-gradient.svg',
+        active: false,
+        navlink: 'settings'
+    }
 ];
