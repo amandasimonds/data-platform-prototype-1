264ce70602ec1183616f275f12a140fe1c1d0034
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-10-07 08:48:22 -0600

    icons

--- uv-ui-lib/projects/uv-ui-lib/src/lib/register-icon/icon-set.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/register-icon/icon-set.ts
@@ -20,20 +20,8 @@ export const ICON_SET = [
 		url: 'apps-sbs.svg'
 	},
     {
-		name: 'apps-sbs-gradient',
-		url: 'gradient/apps-sbs-gradient.svg'
-	},
-    {
-		name: 'uv-logo2',
-		url: 'gradient/uv-logo.svg'
-	},
-    {
 		name: 'apps-settings',
 		url: 'apps-settings.svg'
-	},
-    {
-		name: 'apps-settings-gradient',
-		url: 'gradient/apps-settings-gradient.svg'
 	},
     {
 		name: 'arrow_expand',
@@ -77,7 +65,7 @@ export const ICON_SET = [
 	// },
     {
 		name: 'close',
-		url: 'actions/Download.svg'
+		url: 'states/Blocked.svg'
 	},
     {
 		name: 'compare',
@@ -124,10 +112,6 @@ export const ICON_SET = [
         url: 'fullscreen.svg'
     },
     {
-		name: 'apps-settings-gradient',
-		url: 'gradient/apps-settings-gradient.svg'
-	},
-    {
 		name: 'lightbulb',
 		url: 'lightbulb.svg'
 	},
@@ -167,10 +151,6 @@ export const ICON_SET = [
 		name: 'search',
 		url: 'search.svg'
 	},
-    {
-		name: 'search-gradient',
-		url: 'gradient/apps-search-gradient.svg'
-	},
 	{
 		name: 'settings',
 		url: 'system/Settings.svg'
@@ -190,7 +170,576 @@ export const ICON_SET = [
     {
         name: 'uv-logo',
         url: 'uv-logo.svg'
-    }
+    },
+
+    ///////////////////////////////////////////////////////////////////////////////
+
+    {
+        name: 'activate_all',
+        url: 'actions/Activate all.svg'
+    },
+    {
+        name: 'check_alt',
+        url: 'actions/Check alt.svg'   
+    },
+    {
+        name: 'check',
+        url: 'actions/Check.svg'       
+    },
+    {
+        name: 'clear',
+        url: 'actions/Clear.svg'
+    },
+    {
+        name: 'close',
+        url: 'actions/Close.svg'
+    },
+    {
+        name: 'comment',
+        url: 'actions/Comment.svg'
+    },
+    {
+        name: 'copy',
+        url: 'actions/Copy.svg'
+    },
+    {
+        name: 'delete',
+        url: 'actions/Delete.svg'
+    },
+    {
+        name: 'deselect_all',
+        url: 'actions/Deselect all.svg'
+    },
+    {
+        name: 'document_report',
+        url: 'actions/Document report.svg'
+    },
+    {
+        name: 'document',
+        url: 'actions/Document.svg'
+    },
+    {
+        name: 'documents',
+        url: 'actions/Documents.svg'
+    },
+    {
+        name: 'double_check',
+        url: 'actions/Double check.svg'
+    },
+    {
+        name: 'dowload_report',
+        url: 'actions/Dowload report.svg'
+    },
+    {
+        name: 'download',
+        url: 'actions/Download.svg'
+    },
+    {
+        name: 'dragging',
+        url: 'actions/Dragging.svg'
+    },
+    {
+        name: 'edit_erea',
+        url: 'actions/Edit erea.svg'
+    },
+    {
+        name: 'edit',
+        url: 'actions/Edit.svg'
+    },
+    {
+        name: 'folder_minus',
+        url: 'actions/Folder minus.svg'
+    },
+    {
+        name: 'folder_plus',
+        url: 'actions/Folder plus.svg'
+    },
+    {
+        name: 'hide',
+        url: 'actions/Hide.svg'
+    },
+    {
+        name: 'link',
+        url: 'actions/Link.svg'
+    },
+    {
+        name: 'lock',
+        url: 'actions/Lock.svg'
+    },
+    {
+        name: 'minus',
+        url: 'actions/Minus.svg'
+    },
+    {
+        name: 'pin',
+        url: 'actions/Pin.svg'
+    },
+    {
+        name: 'plus',
+        url: 'actions/Plus.svg'
+    },
+    {
+        name: 'refresh',
+        url: 'actions/Refresh.svg'
+    },
+    {
+        name: 'repeat',
+        url: 'actions/Repeat.svg'
+    },
+    {
+        name: 'replace',
+        url: 'actions/Replace.svg'
+    },
+    {
+        name: 'report_alt',
+        url: 'actions/Report alt.svg'
+    },
+    {
+        name: 'restore_alt',
+        url: 'actions/Restore alt.svg'
+    },
+    {
+        name: 'restore',
+        url: 'actions/Restore.svg'
+    },
+    {
+        name: 'save',
+        url: 'actions/Save.svg'
+    },
+    {
+        name: 'search',
+        url: 'actions/Search.svg'
+    },
+    {
+        name: 'select_all',
+        url: 'actions/Select all.svg'
+    },
+    {
+        name: 'share',
+        url: 'actions/Share.svg'
+    },
+    {
+        name: 'show',
+        url: 'actions/Show.svg'
+    },
+    {
+        name: 'tag',
+        url: 'actions/Tag.svg'
+    },
+    {
+        name: 'thunderbolt',
+        url: 'actions/Thunderbolt.svg'
+    },
+    {
+        name: 'unlock',
+        url: 'actions/Unlock.svg'
+    },
+    {
+        name: 'upload',
+        url: 'actions/Upload.svg'
+    },
+    {
+        name: 'windows',
+        url: 'actions/Windows.svg'
+    },
+    {
+        name: 'filter',
+        url: 'actions/filter.svg'
+    },
+
+    ////////////////////////////////////////////
+
+    {
+        name: 'arrow_back',
+        url: 'navigation/Arrow back.svg'
+    },
+    {
+        name: 'arrow_downrward',
+        url: 'navigation/Arrow downrward.svg'
+    },
+    {
+        name: 'arrow_forward',
+        url: 'navigation/Arrow forward.svg'
+    },
+    {
+        name: 'arrow_uprward',
+        url: 'navigation/Arrow uprward.svg'
+    },
+    {
+        name: 'bottom',
+        url: 'navigation/Bottom.svg'
+    },
+    {
+        name: 'chevron_bottom',
+        url: 'navigation/Chevron bottom.svg'
+    },
+    {
+        name: 'chevron_left',
+        url: 'navigation/Chevron left.svg'
+    },
+    {
+        name: 'chevron_right',
+        url: 'navigation/Chevron right.svg'
+    },
+    {
+        name: 'chevron_top',
+        url: 'navigation/Chevron top.svg'
+    },
+    {
+        name: 'collapse',
+        url: 'navigation/Collapse.svg'
+    },
+    {
+        name: 'double_bottom',
+        url: 'navigation/Double bottom.svg'
+    },
+    {
+        name: 'double_left',
+        url: 'navigation/Double left.svg'
+    },
+    {
+        name: 'double_right',
+        url: 'navigation/Double right.svg'
+    },
+    {
+        name: 'double_top',
+        url: 'navigation/Double top.svg'
+    },
+    {
+        name: 'expand',
+        url: 'navigation/Expand.svg'
+    },
+    {
+        name: 'full_close alt',
+        url: 'navigation/Full close alt.svg'
+    },
+    {
+        name: 'full_open alt',
+        url: 'navigation/Full open alt.svg'
+    },
+    {
+        name: 'full_screen exit',
+        url: 'navigation/Full screen exit.svg'
+    },
+    {
+        name: 'full_screen',
+        url: 'navigation/Full screen.svg'
+    },
+    {
+        name: 'left',
+        url: 'navigation/Left.svg'
+    },
+    {
+        name: 'menu',
+        url: 'navigation/Menu.svg'
+    },
+    {
+        name: 'right',
+        url: 'navigation/Right.svg'
+    },
+    {
+        name: 'scroll_bottom',
+        url: 'navigation/Scroll bottom.svg'
+    },
+    {
+        name: 'scroll_top',
+        url: 'navigation/Scroll top.svg'
+    },
+    {
+        name: 'top',
+        url: 'navigation/Top.svg'
+    },
+
+    ///////////////////////////
+
+    {
+        name: 'blocked',
+        url: 'states/Blocked.svg'
+    },
+    {
+        name: 'check',
+        url: 'states/Check.svg'
+    },
+    {
+        name: 'double_check',
+        url: 'states/Double check.svg'
+    },
+    {
+        name: 'empty',
+        url: 'states/Empty.svg'
+    },
+    {
+        name: 'link',
+        url: 'states/Link.svg'
+    },
+    {
+        name: 'loader',
+        url: 'states/Loader.svg'
+    },
+    {
+        name: 'loading',
+        url: 'states/Loading.svg'
+    },
+    {
+        name: 'unlink_alt',
+        url: 'states/Unlink alt.svg'
+    },
+    {
+        name: 'unlink',
+        url: 'states/Unlink.svg'
+    },
+
+    //////////////////////////////////
+    {
+        name: 'calendar',
+        url: 'content/Calendar.svg'
+    },
+    {
+        name: 'clock',
+        url: 'content/Clock.svg'
+    },
+    {
+        name: 'clone',
+        url: 'content/Clone.svg'
+    },
+    {
+        name: 'collection',
+        url: 'content/Collection.svg'
+    },
+    {
+        name: 'collections',
+        url: 'content/Collections.svg'
+    },
+    {
+        name: 'devider_out',
+        url: 'content/Devider out.svg'
+    },
+    {
+        name: 'drop_down',
+        url: 'content/Drop down.svg'
+    },
+    {
+        name: 'drop_left down',
+        url: 'content/Drop left down.svg'
+    },
+    {
+        name: 'drop_left up',
+        url: 'content/Drop left up.svg'
+    },
+    {
+        name: 'drop_left',
+        url: 'content/Drop left.svg'
+    },
+    {
+        name: 'drop_right down',
+        url: 'content/Drop right down.svg'
+    },
+    {
+        name: 'drop_right up',
+        url: 'content/Drop right up.svg'
+    },
+    {
+        name: 'drop_right',
+        url: 'content/Drop right.svg'
+    },
+    {
+        name: 'drop_up',
+        url: 'content/Drop up.svg'
+    },
+    {
+        name: 'error',
+        url: 'content/Error.svg'
+    },
+    {
+        name: 'filter',
+        url: 'content/Filter.svg'
+    },
+    {
+        name: 'folder_opened',
+        url: 'content/Folder opened.svg'
+    },
+    {
+        name: 'folder',
+        url: 'content/Folder.svg'
+    },
+    {
+        name: 'forced_directed graph',
+        url: 'content/Forced directed graph.svg'
+    },
+    {
+        name: 'list_view',
+        url: 'content/List view.svg'
+    },
+    {
+        name: 'more_horisontal',
+        url: 'content/More horisontal.svg'
+    },
+    {
+        name: 'more_vertical',
+        url: 'content/More vertical.svg'
+    },
+    {
+        name: 'profile',
+        url: 'content/Profile.svg'
+    },
+    {
+        name: 'redo_alt',
+        url: 'content/Redo alt.svg'
+    },
+    {
+        name: 'redo',
+        url: 'content/Redo.svg'
+    },
+    {
+        name: 'search_results alt',
+        url: 'content/Search results alt.svg'
+    },
+    {
+        name: 'search_results',
+        url: 'content/Search results.svg'
+    },
+    {
+        name: 'section',
+        url: 'content/Section.svg'
+    },
+    {
+        name: 'settings',
+        url: 'content/Settings.svg'
+    },
+    {
+        name: 'sorting_alt',
+        url: 'content/Sorting alt.svg'
+    },
+    {
+        name: 'sorting_bottom',
+        url: 'content/Sorting bottom.svg'
+    },
+    {
+        name: 'sorting_top',
+        url: 'content/Sorting top.svg'
+    },
+    {
+        name: 'sorting',
+        url: 'content/Sorting.svg'
+    },
+    {
+        name: 'success',
+        url: 'content/Success.svg'
+    },
+    {
+        name: 'tune',
+        url: 'content/Tune.svg'
+    },
+    {
+        name: 'undo_alt',
+        url: 'content/Undo alt.svg'
+    },
+    {
+        name: 'undo',
+        url: 'content/Undo.svg'
+    },
+    {
+        name: 'user',
+        url: 'content/User.svg'
+    },
+    {
+        name: 'users',
+        url: 'content/Users.svg'
+    },
+    {
+        name: 'wave',
+        url: 'content/Wave.svg'
+    },
+
+    /////////////////////////////////////
+
+    {
+        name: 'analytics_alt',
+        url: 'services/Analytics alt.svg'
+    },
+    {
+        name: 'analytics',
+        url: 'services/Analytics.svg'
+    },
+    {
+        name: 'compare',
+        url: 'services/Compare.svg'
+    },
+    {
+        name: 'dashboard',
+        url: 'services/Dashboard.svg'
+    },
+    {
+        name: 'design',
+        url: 'services/Design.svg'
+    },
+    {
+        name: 'global_where used',
+        url: 'services/Global where used.svg'
+    },
+    {
+        name: 'help',
+        url: 'services/Help.svg'
+    },
+    {
+        name: 'ingestion',
+        url: 'services/Ingestion.svg'
+    },
+    {
+        name: 'package',
+        url: 'services/Package.svg'
+    },
+    {
+        name: 'projects',
+        url: 'services/Projects.svg'
+    },
+    {
+        name: 'search',
+        url: 'services/Search.svg'
+    },
+    {
+        name: 'services',
+        url: 'services/Services.svg'
+    },
+    {
+        name: 'settings',
+        url: 'services/Settings.svg'
+    },
+    {
+        name: 'shorcuts',
+        url: 'services/Shorcuts.svg'
+    },
+    {
+        name: 'store',
+        url: 'services/Store.svg'
+    },
+    {
+        name: 'tasks_alt',
+        url: 'services/Tasks alt.svg'
+    },
+    {
+        name: 'tasks',
+        url: 'services/Tasks.svg'
+    },
+    
+    //////////////////////////////////
+
+    {
+        name: 'directed_graph',
+        url: 'gwu/Directed graph.svg'
+    },
+    {
+        name: 'gwu-profile',
+        url: 'gwu/Profile.svg'
+    },
+    {
+        name: 'search_results',
+        url: 'gwu/Search results.svg'
+    },
+    {
+        name: 'waves',
+        url: 'gwu/Waves.svg'
+    },
 ] as const;
 
 export type UvxMatIconName = typeof ICON_SET[number]['name'];
