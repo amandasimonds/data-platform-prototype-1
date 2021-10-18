4ad17bdb526de7a4b5053bf5c0dc1db03491aab8
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-10-18 17:11:28 -0600

    updated icons, added epd pattern library, updated headers, breakpoints, grid changes, profile viewers and columns, new styles on buttons

--- uv-ui-lib/projects/uv-ui-prototype/src/app/global-where-used/profile-viewer-container/profile-viewer-container.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/global-where-used/profile-viewer-container/profile-viewer-container.component.ts
@@ -1,4 +1,7 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, ChangeDetectorRef, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { NgOnDestroyService } from '../../services/on-destroy.service';
+import { ProfileViewerService } from '../../services/profile-viewer.service';
 
 @Component({
   selector: 'app-profile-viewer-container',
@@ -6,6 +9,22 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./profile-viewer-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileViewerContainerComponent {
+export class ProfileViewerContainerComponent implements OnInit {
+
+    @Input() public columnCount = 2;
+
+    constructor(
+        private profileViewerService: ProfileViewerService, 
+        private ref: ChangeDetectorRef,
+        private destroy$ : NgOnDestroyService) {}
+
+    ngOnInit(): void {
+        this.profileViewerService.columnCount
+        .pipe(takeUntil(this.destroy$))
+            .subscribe(count => {
+                this.columnCount = count;
+                this.ref.detectChanges();
+            });
+    }
 
 }
