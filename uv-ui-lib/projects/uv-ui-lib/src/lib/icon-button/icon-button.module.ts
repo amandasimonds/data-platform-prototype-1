dd6040e3b703b42b6d01556d8ce29a5c3e89de6b
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2021-10-25 14:38:29 -0600

    broke out step one of wizard to its own component

--- uv-ui-lib/projects/uv-ui-lib/src/lib/icon-button/icon-button.module.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/icon-button/icon-button.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterIconModule } from '../register-icon/register-icon.module';
 import { IconButtonComponent } from './icon-button.component';
+import { UvxIconModule } from '../uvx-icon/uvx-icon.module';
 
 @NgModule({
     declarations: [ IconButtonComponent ],
     imports: [
         CommonModule,
-        RegisterIconModule
+        RegisterIconModule,
+        UvxIconModule
     ],
     exports: [ IconButtonComponent ]
 })
--- uv-ui-lib/projects/uv-ui-prototype/src/app/global-where-used/project-wizard/step-directions/step-one/step-one.component.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/global-where-used/project-wizard/step-directions/step-one/step-one.component.ts
@@ -1,4 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { StepsService } from 'projects/uv-ui-prototype/src/app/services/steps.service';
 
 @Component({
   selector: 'app-step-one',
@@ -7,5 +9,73 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StepOneComponent {
+
+    public selectedRole = "Select a role";
+
+    public roles = [
+        {
+            name: 'Role 1'
+        },
+        {
+            name: 'Role 2'
+        },
+        {
+            name: 'Role 3'
+        },
+        {
+            name: 'Role 4'
+        }
+    ];
+
+    
+    public taskSelections = [
+        {
+            name: 'Compare',
+            icon: 'compare',
+            selected: false
+        },
+        {
+            name: 'Current',
+            icon: 'thunderbolt',
+            selected: false
+        },
+        {
+            name: 'Design',
+            icon: 'package',
+            selected: false
+        },
+        {
+            name: 'Replace',
+            icon: 'replace',
+            selected: false
+        },
+        {
+            name: 'Similar',
+            icon: 'clone',
+            selected: false
+        },
+        {
+            name: 'Update',
+            icon: 'refresh',
+            selected: false
+        },
+        {
+            name: 'Validate',
+            icon: 'check_alt',
+            selected: false
+        },
+        {
+            name: 'Test',
+            icon: 'activate_all',
+            selected: false
+        }
+    ];
+
+    constructor(private stepsService: StepsService) {}
+
+    public selectRole(role: string) {
+        this.selectedRole = role;
+        this.stepsService.updateWizardData('role', role)
+    }
 
 }
