6fd91f3c2d016e84c30a4485acc407e8e9bec0b1
Amanda Simonds <amanda.simonds@ihsmarkit.com>
2022-06-06 21:37:19 +0000

    Merged PR 62171: File Folder Structure
    File folder structure service added
    
    Related work items: #423122

--- uv-ui-lib/projects/uv-ui-lib/src/lib/selectable-entity/selectable-entity.module.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/selectable-entity/selectable-entity.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectableEntityComponent } from './selectable-entity.component';
-// import { CheckboxModule } from 'projects/uv-ui-lib/src/public-api';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CheckboxModule } from '../checkbox/checkbox.module';
 
 
 @NgModule({
   declarations: [SelectableEntityComponent],
   imports: [
     CommonModule,
-    // CheckboxModule,
+    CheckboxModule,
     DragDropModule
   ],
   exports: [SelectableEntityComponent]
