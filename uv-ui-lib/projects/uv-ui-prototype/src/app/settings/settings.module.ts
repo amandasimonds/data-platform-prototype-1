4f8141e389f46b2dc6a3c02cc9f18c76e8b4eeb7
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-08 12:30:40 -0400

    updated last angular package, deleted old packages and services

--- uv-ui-lib/projects/uv-ui-prototype/src/app/settings/settings.module.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/settings/settings.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { ConnectorTableComponent } from './connector-table/connector-table.component';
-import { ButtonDropdownModule } from 'epd-pattern-library';
 import { ActionBarModule, ButtonModule, DropdownModule, ExpandingMenuModule, IconButtonModule, NavbarSubModule, TextInputModule, TooltipModule, UvxIconModule } from 'projects/uv-ui-lib/src/public-api';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsMenuModule } from './settings-menu/settings-menu.module';
@@ -21,7 +20,6 @@ import { SettingsSubNavbarComponent } from './settings-sub-navbar/settings-sub-n
   imports: [
     CommonModule,
     ButtonModule,
-    ButtonDropdownModule,
     DropdownModule,
     IconButtonModule,
     SettingsRoutingModule,
