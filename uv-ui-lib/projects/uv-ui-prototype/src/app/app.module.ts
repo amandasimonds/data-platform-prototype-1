85f5d31ff036164ce2e39a8f75575c2a40bbfae7
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-08 18:32:35 -0400

    rename auth file

--- uv-ui-lib/projects/uv-ui-prototype/src/app/app.module.ts
+++ uv-ui-lib/projects/uv-ui-prototype/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core'
 // import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import {
     ActionBarModule,
     ButtonModule,
@@ -21,46 +21,46 @@ import {
     ExpandingMenuModule,
     NavbarSubModule,
     ToastMessageModule,
-    ICON_SET
-} from '../../../uv-ui-lib/src/public-api';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { HomeSplashModule } from './pages/home-splash/home-splash.module';
-import { SettingsModule } from './settings/settings.module';
-import { SettingsMenuModule } from './settings/settings-menu/settings-menu.module';
-import { MainComponent } from './layouts/main/main.component';
-import { HelperService } from './services/helper.service';
-import { SearchService } from './services/search.service';
-import { SideBySideModule } from './side-by-side/side-by-side.module';
-import { SidebySideService } from './services/side-by-side.service';
-import { NgOnDestroyService } from './services/on-destroy.service';
-import { BackdropService } from './services/backdrop.service';
-import { AppShellService } from './services/app-shell.service';
-import { SearchSidebarModule } from './search/search-sidebar/search-sidebar.module';
-import { CompareWarningModule } from './search/compare-warning/compare-warning.module';
-import { UvLightComponent } from './uv-light/uv-light.component';
-import { UserService } from './auth/user.service';
-import { UvLightService } from './services/uv-light.service';
-import { IconDisplayComponent } from './pages/icon-display/icon-display.component';
-import { ProfileViewerService } from './global-where-used/profile-viewer.service';
-import { NgxEchartsModule } from 'ngx-echarts';
-import * as echarts from 'echarts';
-import { WalletModule } from './wallet/wallet.module';
-import { DragDropService } from './services/drag-drop.service';
-import { ConnectorCreatorService } from './services/connector-creator.service';
-import { WalletService } from './services/wallet.service';
-import { DevAuthGuardService } from './auth/dev-auth-guard.service';
-import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
-import { ClickOutsideModule } from './shared/directives/click-outside/click-outside.module';
-import { CustomerNavbarComponent } from './layouts/main/customer-navbar/customer-navbar.component';
-import { DevNavbarComponent } from './layouts/main/dev-navbar/dev-navbar.component';
-import { FileFolderStructureModule } from './file-folder-structure/file-folder-structure.module';
-import { FileFolderStructureService } from './services/file-folder.service';
-import { BreadcrumbService } from 'xng-breadcrumb';
-import { ToastMessageService } from './services/toast-message.service';
-import { DevUTAuthGuardService } from './auth/DevUT-auth-guard.service copy';
-import { ProfileSlideOutModule } from './side-by-side/profile-slide-out/profile-slide-out.module';
+    ICON_SET,
+} from '../../../uv-ui-lib/src/public-api'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { LoginComponent } from './pages/login/login.component'
+import { HomeSplashModule } from './pages/home-splash/home-splash.module'
+import { SettingsModule } from './settings/settings.module'
+import { SettingsMenuModule } from './settings/settings-menu/settings-menu.module'
+import { MainComponent } from './layouts/main/main.component'
+import { HelperService } from './services/helper.service'
+import { SearchService } from './services/search.service'
+import { SideBySideModule } from './side-by-side/side-by-side.module'
+import { SidebySideService } from './services/side-by-side.service'
+import { NgOnDestroyService } from './services/on-destroy.service'
+import { BackdropService } from './services/backdrop.service'
+import { AppShellService } from './services/app-shell.service'
+import { SearchSidebarModule } from './search/search-sidebar/search-sidebar.module'
+import { CompareWarningModule } from './search/compare-warning/compare-warning.module'
+import { UvLightComponent } from './uv-light/uv-light.component'
+import { UserService } from './auth/user.service'
+import { UvLightService } from './services/uv-light.service'
+import { IconDisplayComponent } from './pages/icon-display/icon-display.component'
+import { ProfileViewerService } from './global-where-used/profile-viewer.service'
+import { NgxEchartsModule } from 'ngx-echarts'
+import * as echarts from 'echarts'
+import { WalletModule } from './wallet/wallet.module'
+import { DragDropService } from './services/drag-drop.service'
+import { ConnectorCreatorService } from './services/connector-creator.service'
+import { WalletService } from './services/wallet.service'
+import { DevAuthGuardService } from './auth/dev-auth-guard.service'
+import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component'
+import { ClickOutsideModule } from './shared/directives/click-outside/click-outside.module'
+import { CustomerNavbarComponent } from './layouts/main/customer-navbar/customer-navbar.component'
+import { DevNavbarComponent } from './layouts/main/dev-navbar/dev-navbar.component'
+import { FileFolderStructureModule } from './file-folder-structure/file-folder-structure.module'
+import { FileFolderStructureService } from './services/file-folder.service'
+import { BreadcrumbService } from 'xng-breadcrumb'
+import { ToastMessageService } from './services/toast-message.service'
+import { DevUTAuthGuardService } from './auth/dev-ut-auth-guard.service'
+import { ProfileSlideOutModule } from './side-by-side/profile-slide-out/profile-slide-out.module'
 // import { AuthCallbackComponent } from './pages/auth-callback/auth-callback.component';
 
 @NgModule({
@@ -72,7 +72,7 @@ import { ProfileSlideOutModule } from './side-by-side/profile-slide-out/profile-
         IconDisplayComponent,
         UnauthorizedComponent,
         CustomerNavbarComponent,
-        DevNavbarComponent
+        DevNavbarComponent,
         // AuthCallbackComponent
     ],
     imports: [
@@ -98,7 +98,7 @@ import { ProfileSlideOutModule } from './side-by-side/profile-slide-out/profile-
         NavbarSubModule,
         ExpandingMenuModule,
         NgxEchartsModule.forRoot({
-            echarts: () => import('echarts')
+            echarts: () => import('echarts'),
         }),
         ProfileSlideOutModule,
         RegisterIconModule,
@@ -111,7 +111,7 @@ import { ProfileSlideOutModule } from './side-by-side/profile-slide-out/profile-
         TabModule,
         ToastMessageModule,
         UvxIconModule,
-        WalletModule
+        WalletModule,
     ],
     providers: [
         AppShellService,
@@ -131,8 +131,9 @@ import { ProfileSlideOutModule } from './side-by-side/profile-slide-out/profile-
         UvLightService,
         WalletService,
         FileFolderStructureService,
-        BreadcrumbService
+        BreadcrumbService,
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
