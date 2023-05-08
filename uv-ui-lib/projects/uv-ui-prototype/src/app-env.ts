0bd42eb9466a2a2a74c4971834b4cf1a156a26ed
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-08 18:25:46 -0400

    moved file

---
+++ uv-ui-lib/projects/uv-ui-prototype/src/app-env.ts
@@ -0,0 +1,6 @@
+export const ENV = {
+    ut_authority: 'https://dev-0d2xh410.us.auth0.com',
+    ut_client_id: 'GAb6wGt0e5Sa5HmmyLwVEVDNRXQ61Bwv',
+    dev_authority: 'https://uv-dev-test.us.auth0.com',
+    dev_client_id: 'g3ijY9qhNYluzQOQVnnc1hyYi0uxtoXq',
+} 
