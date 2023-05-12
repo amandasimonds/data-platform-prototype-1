8cc0ee1383d2ca66955749451e42df6aa19fc5b2
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-12 11:26:35 -0400

    try again

--- docs/main.bcfb17c877799df7.js
+++ docs/main.bcfb17c877799df7.js
@@ -4395,7 +4395,7 @@ __webpack_require__.r(__webpack_exports__);
 
 const routes = [{
   path: '',
-  redirectTo: 'main'
+  redirectTo: 'data-platform-prototype-1/main'
 }, {
   path: 'unauthorized',
   component: _pages_unauthorized_unauthorized_component__WEBPACK_IMPORTED_MODULE_2__.UnauthorizedComponent
