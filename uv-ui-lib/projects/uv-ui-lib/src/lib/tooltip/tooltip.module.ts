adac4ca9f4d6c89a45dcf7d507416c080c3181dd
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-18 17:53:41 -0400

    fixed tooltips by adding in angular styling

--- uv-ui-lib/projects/uv-ui-lib/src/lib/tooltip/tooltip.module.ts
+++ uv-ui-lib/projects/uv-ui-lib/src/lib/tooltip/tooltip.module.ts
@@ -4,8 +4,8 @@ import { MatTooltipDefaultOptions, MatTooltipModule, MAT_TOOLTIP_DEFAULT_OPTIONS
 
 export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
     showDelay: 500,
-    hideDelay: 100,
-    touchendHideDelay: 100,
+    hideDelay: 0,
+    touchendHideDelay: 0,
   };
 
 @NgModule({
